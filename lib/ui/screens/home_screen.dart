b2457fbcbeadcc0f1248b19eabbf9e9f8894c728
Rydmike <39990307+rydmike@users.noreply.github.com>
2024-07-01 17:15:15 +0300

    Home screen list view without builder, it is small enough, keep it simple

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -42,145 +42,6 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    final ThemeData theme = Theme.of(context);
-    final bool isDark = theme.brightness == Brightness.dark;
-    // The widgets we want to show in a long list of widgets.
-    // Their style will need to be updated when the theme changes.
-    content = <Widget>[
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0),
-        child: Text(
-          'This app demonstrates platform adaptive theming and other '
-          'advanced theming features and shows the result.',
-        ),
-      ),
-      const SizedBox(height: 8),
-      ListTile(
-        title: const Text('Theme mode'),
-        subtitle: Text('Using ${widget.settings.themeMode.name}'),
-        trailing: ThemeModeSwitch(
-          themeMode: widget.settings.themeMode,
-          onChanged: (ThemeMode mode) {
-            widget.onSettings(
-              widget.settings.copyWith(themeMode: mode),
-            );
-          },
-        ),
-        onTap: () {
-          if (isDark) {
-            widget.onSettings(
-              widget.settings.copyWith(themeMode: ThemeMode.light),
-            );
-          } else {
-            widget.onSettings(
-              widget.settings.copyWith(themeMode: ThemeMode.dark),
-            );
-          }
-        },
-      ),
-      const SizedBox(height: 8),
-      SwitchListTile(
-        title: const Text('Use Material 3'),
-        value: widget.settings.useMaterial3,
-        onChanged: (bool value) {
-          widget.onSettings(
-            widget.settings.copyWith(useMaterial3: value),
-          );
-        },
-      ),
-      SwitchListTile(
-        title: const Text('Zoom blog fonts theme'),
-        value: widget.settings.zoomBlogFonts,
-        onChanged: (bool value) {
-          widget.onSettings(
-            widget.settings.copyWith(zoomBlogFonts: value),
-          );
-        },
-      ),
-      ListTile(
-        title: const Text('Page with colors and themed widgets'),
-        trailing: const Icon(Icons.arrow_forward_ios),
-        onTap: () async {
-          await ComponentsDemoScreen.show(context);
-        },
-      ),
-      // Custom widget token based style, no theme animation.
-      const OrderStatesCard(useTheme: false),
-      // Custom widget theme extension based styles, has theme animation.
-      const OrderStatesCard(useTheme: true),
-      // Blog posts demo with theme extension based TextStyles, theme animation.
-      const BlogPostCard(
-          cardHeading: 'Blog Example',
-          blogHeading: BlogTexts.blogHeading1,
-          blogContent: BlogTexts.blogContent1),
-      const BlogPostCard(
-          cardHeading: 'Another Blog Example',
-          blogHeading: BlogTexts.blogHeading2,
-          blogContent: BlogTexts.blogContent2),
-      // Show the active ColorScheme
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: ShowColorScheme(),
-      ),
-      // Show various Material widgets
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: ButtonsSwitchesIconsShow(),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: ToggleFabSwitchesChipsShow(),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: TextInputFieldShow(),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: AppTabBottomSearch(),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: BottomNavigationBarsShow(),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: NavigationRailShow(),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: NavigationDrawerShow(),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: DialogShow(),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: BottomSheetSnackBannerShow(),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: CardsShow(),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: ListTileShow(),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: TextThemeShow(),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
-        child: PrimaryTextThemeShow(),
-      ),
-    ];
-  }
-
-  @override
   void dispose() {
     scrollController.dispose();
     super.dispose();
@@ -188,12 +49,13 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final bool isDark = theme.brightness == Brightness.dark;
     final Size mediaSize = MediaQuery.sizeOf(context);
     final EdgeInsets mediaPadding = MediaQuery.paddingOf(context);
     final double margins = App.responsiveInsets(mediaSize.width);
     final double topPadding = mediaPadding.top + kToolbarHeight + margins;
     final double bottomPadding = mediaPadding.bottom + margins;
-
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: customOverlayStyle(),
       child: SafeArea(
@@ -206,9 +68,7 @@ class _HomePageState extends State<HomePage> {
               ThemeModeIconButton(
                 themeMode: widget.settings.themeMode,
                 onChanged: (ThemeMode mode) {
-                  widget.onSettings(
-                    widget.settings.copyWith(themeMode: mode),
-                  );
+                  widget.onSettings(widget.settings.copyWith(themeMode: mode));
                 },
               ),
               const AboutIconButton(),
@@ -219,13 +79,140 @@ class _HomePageState extends State<HomePage> {
           body: ResponsiveCenter(
             controller: scrollController,
             maxContentWidth: App.maxBodyWidth,
-            child: ListView.builder(
-                controller: scrollController,
-                padding: EdgeInsets.fromLTRB(0, topPadding, 0, bottomPadding),
-                itemCount: content.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return content.elementAt(index);
-                }),
+            child: ListView(
+              controller: scrollController,
+              padding: EdgeInsets.fromLTRB(0, topPadding, 0, bottomPadding),
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    'This app demonstrates platform adaptive theming and '
+                    'other advanced theming features and shows the result.',
+                  ),
+                ),
+                const SizedBox(height: 8),
+                ListTile(
+                  title: const Text('Theme mode'),
+                  subtitle: Text('Using ${widget.settings.themeMode.name}'),
+                  trailing: ThemeModeSwitch(
+                    themeMode: widget.settings.themeMode,
+                    onChanged: (ThemeMode mode) {
+                      widget.onSettings(
+                        widget.settings.copyWith(themeMode: mode),
+                      );
+                    },
+                  ),
+                  onTap: () {
+                    if (isDark) {
+                      widget.onSettings(
+                        widget.settings.copyWith(themeMode: ThemeMode.light),
+                      );
+                    } else {
+                      widget.onSettings(
+                        widget.settings.copyWith(themeMode: ThemeMode.dark),
+                      );
+                    }
+                  },
+                ),
+                const SizedBox(height: 8),
+                SwitchListTile(
+                  title: const Text('Use Material 3'),
+                  value: widget.settings.useMaterial3,
+                  onChanged: (bool value) {
+                    widget.onSettings(
+                      widget.settings.copyWith(useMaterial3: value),
+                    );
+                  },
+                ),
+                SwitchListTile(
+                  title: const Text('Zoom blog fonts theme'),
+                  value: widget.settings.zoomBlogFonts,
+                  onChanged: (bool value) {
+                    widget.onSettings(
+                      widget.settings.copyWith(zoomBlogFonts: value),
+                    );
+                  },
+                ),
+                ListTile(
+                  title: const Text('Page with colors and themed widgets'),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  onTap: () async {
+                    await ComponentsDemoScreen.show(context);
+                  },
+                ),
+                // Token based style, no theme animation.
+                const OrderStatesCard(useTheme: false),
+                // Theme extension based styles, has theme animation.
+                const OrderStatesCard(useTheme: true),
+                // Demo text style theme extension, has size change animation.
+                const BlogPostCard(
+                    cardHeading: 'Blog Example',
+                    blogHeading: BlogTexts.blogHeading1,
+                    blogContent: BlogTexts.blogContent1),
+                const BlogPostCard(
+                    cardHeading: 'Another Blog Example',
+                    blogHeading: BlogTexts.blogHeading2,
+                    blogContent: BlogTexts.blogContent2),
+                // Show the active ColorScheme
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: ShowColorScheme(),
+                ),
+                // Show various Material widgets
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: ButtonsSwitchesIconsShow(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: ToggleFabSwitchesChipsShow(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: TextInputFieldShow(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: AppTabBottomSearch(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: BottomNavigationBarsShow(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: NavigationRailShow(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: NavigationDrawerShow(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: DialogShow(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: BottomSheetSnackBannerShow(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: CardsShow(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: ListTileShow(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: TextThemeShow(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+                  child: PrimaryTextThemeShow(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
